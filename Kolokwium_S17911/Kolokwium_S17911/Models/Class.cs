d73cd28053236933327c16caab16484207eec3ba
s17911 <61651003+s17911@users.noreply.github.com>
2020-06-21 20:50:09 +0200

    puste kolokwium

---
+++ Kolokwium_S17911/Kolokwium_S17911/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium_S17911.Models
+{
+    public class Class
+    {
+    }
+}
