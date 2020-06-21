10523405544c890757ddc53726d98930cb53da1f
s17911 <61651003+s17911@users.noreply.github.com>
2020-06-21 20:49:13 +0200

    przykl kol na pkt

---
+++ przykladadowe_kol/s17911Kolokwium/Models/ZamowienieProdukt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace s17911Kolokwium.Models
+{
+    public class ZamowienieProdukt
+    {
+        
+        [Required]
+        public int Quantity { get; set; }
+        [MaxLength(300)]
+        public string Comments { get; set; }
+
+        [Key]
+        [ForeignKey("WyrobProdukt")]
+        public int IdConfectioneryProduct { get; set; }
+        public WyrobProdukt ConfectioneryProduct { get; set; }
+
+        [Key]
+        [ForeignKey("Zamowienie")]
+        public int IdOrder { get; set; }
+        public Zamowienie Order { get; set; }
+    }
+}
